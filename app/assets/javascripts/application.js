c6b5a03a37a7d1320a995655a3daa9eab0d0f348
Bin Zhu <binzhu1070@gmail.com>
2012-08-08 13:23:46 -0400

    test

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,21 +13,3 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
-
-$(document).ready(function(){
-    //alert("application js called!");
-    $.ajax({
-        
-        
-        
-    })// ajax call to get
-    
-    
-    
-    $(".ancmts a").click(function(){
-    //alert("alert!");
-    $(this).parent().fadeOut();
-    return false;
-    })//click function end
-
-})
