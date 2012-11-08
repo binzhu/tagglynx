8ed6c18667557f9c08b52d495bfb4d4d4c5188e0
Bin Zhu <binzhu1070@gmail.com>
2012-11-08 10:55:28 -0800

    login redirect fix

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,14 +4,12 @@ class AdminController < ApplicationController
       user = User.find_by_username(params[:username])
       if user && user.pwd == params[:pwd]
         session[:user_id] = user.id
-          localserver = 'http://localhost:3000'
-          rackspaceserver = 'http://198.101.236.20:3000'
-          
+          server = Rails.env == "development" ? 'http://localhost:3000':'http://198.101.236.20:3000'
         if session[:url].nil? || session[:title].nil?
-          redirect_to localserver # if this happens, session of url could not be set because no params is passed to index
+          redirect_to server # if this happens, session of url could not be set because no params is passed to index
           return                      # by default this should not happen since iframe is always loaded from when there is page open
         else
-          redirect_to localserver + '?url='+session[:url] + '&title=' + session[:title]
+          redirect_to server + '?url='+session[:url] + '&title=' + session[:title]
           return
         end
       else
